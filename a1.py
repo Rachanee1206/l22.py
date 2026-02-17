7a2881bee579e49eb2d32ddccf789092e20e8c59
Rachanee1206 <rachaneeb.t.r1206@gmail.com>
2026-02-17 19:59:54 +0700

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,6 @@
+class student :
+   grade=8
+   name="john"
+   print("Hi, My name is ",name,"and I am in grade ",grade)
+
+obj=student()
---
+++ a2.py
@@ -0,0 +1,15 @@
+class vehicle:
+  def __init__(self, name, max_speed, mileage):
+      self.name = name
+      self.max_speed = max_speed
+      self.mileage = mileage
+
+bmw=vehicle("BMW",200,100)
+print("vehicle name:",bmw. name)
+print("vehicle max speed:",bmw.max_speed)
+print("vehicle mileage:",bmw.mileage)
+print("\n")
+audi=vehicle("Audi",250,150)
+print("vehicle name:", audi. name)
+print("vehicle max speed: ",audi.max_speed)
+print("vehicle mileage:",audi.mileage)
